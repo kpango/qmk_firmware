8d5c730b0a34d9b269fe1358f9f16ec2d6d82b9d
kpango <i.can.feel.gravity@gmail.com>
2018-05-07 16:26:33 +0900

    fix

--- keyboards/ergo42/keymaps/biacco/keymap.c
+++ keyboards/ergo42/keymaps/biacco/keymap.c
@@ -22,16 +22,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
  * | Sft  |   Z  |   X  |   C  |   V  |   B  |  {   |   |  }   |   N  |   M  |   ,  |   .  |   /  |  \   |
  * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
- * | Ctrl | GUI  |  App |PrtSc |ESC/  |Space/|Tab/  |   |Back  |Enter/| Del  |PrtSc | GUI  |=>SYMB|  \   |
+ * | Ctrl | GUI  |  App |PrtSc |ESC/  |Space/|Tab/  |   |Back  |Enter/| Del  |   +  |   -  |  *   |  =   |
  * |      |      |      |      |~SYMB |RCtrl |Shift |   |Space |~META |      |      |      |      |      |
  * `------------------------------------------------'   `------------------------------------------------'
  */
 
 [BASE] = KEYMAP( \
-  KC_ESC,   KC_Q,    KC_W,   KC_E,    KC_R,             KC_T,           KC_LBRC,       KC_RBRC,    KC_Y,             KC_U,    KC_I,     KC_O,     KC_P,     KC_MINS, \
-  KC_LALT,  KC_A,    KC_S,   KC_D,    KC_F,             KC_G,           S(KC_9),       S(KC_0),    KC_H,             KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
-  KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,             KC_B,           S(KC_LBRC),    S(KC_RBRC), KC_N,             KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_BSLS, \
-  KC_LCTRL, KC_LGUI, KC_APP, KC_PSCR, LT(SYMB, KC_ESC), RCTL_T(KC_SPC), SFT_T(KC_TAB), KC_BSPC,    LT(META, KC_ENT), KC_DELT, KC_PSCR,  KC_RGUI,  TG(SYMB), KC_JYEN \
+  KC_ESC,   KC_Q,    KC_W,    KC_E,    KC_R,             KC_T,           KC_LBRC,       KC_RBRC,    KC_Y,             KC_U,    KC_I,     KC_O,     KC_P,     KC_MINS, \
+  KC_LALT,  KC_A,    KC_S,    KC_D,    KC_F,             KC_G,           S(KC_9),       S(KC_0),    KC_H,             KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
+  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,             KC_B,           S(KC_LBRC),    S(KC_RBRC), KC_N,             KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_BSLS, \
+  KC_LCTRL, KC_LGUI, KC_EXLM, S(KC_1), LT(SYMB, KC_ESC), RCTL_T(KC_TAB), SFT_T(KC_SPC), KC_BSPC,    LT(META, KC_ENT), KC_DEL,  KC_AMPL,  KC_PERC,  KC_ASTR,  KC_EQL \
 ),
 
 /* META
