25f30440422a5b53c856bc9d91fcba9bfd0c436b
kpango <i.can.feel.gravity@gmail.com>
2018-05-07 16:29:36 +0900

    fix

--- keyboards/ergo42/keymaps/biacco/keymap.c
+++ keyboards/ergo42/keymaps/biacco/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_Q,    KC_W,    KC_E,    KC_R,             KC_T,           KC_LBRC,       KC_RBRC,    KC_Y,             KC_U,    KC_I,     KC_O,     KC_P,     KC_MINS, \
   KC_LALT,  KC_A,    KC_S,    KC_D,    KC_F,             KC_G,           S(KC_9),       S(KC_0),    KC_H,             KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,             KC_B,           S(KC_LBRC),    S(KC_RBRC), KC_N,             KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_BSLS, \
-  KC_LCTRL, KC_LGUI, KC_EXLM, S(KC_1), LT(SYMB, KC_ESC), RCTL_T(KC_TAB), SFT_T(KC_SPC), KC_BSPC,    LT(META, KC_ENT), KC_DEL,  KC_AMPL,  KC_PERC,  KC_ASTR,  KC_EQL \
+  KC_LCTRL, KC_LGUI, KC_EXLM, S(KC_SLSH), LT(SYMB, KC_ESC), RCTL_T(KC_TAB), SFT_T(KC_SPC), KC_BSPC,    LT(META, KC_ENT), KC_DEL,  KC_AMPR,  KC_PERC,  KC_ASTR,  KC_EQL \
 ),
 
 /* META
