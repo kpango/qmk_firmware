724b53ab152b2c68bc2d99ec5aed2180a4abdc61
kpango <i.can.feel.gravity@gmail.com>
2018-05-08 13:54:27 +0900

    fix

--- keyboards/ergo42/keymaps/biacco/keymap.c
+++ keyboards/ergo42/keymaps/biacco/keymap.c
@@ -18,20 +18,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,------------------------------------------------.   ,------------------------------------------------.
  * | Esc  |   Q  |   W  |   E  |   R  |   T  |  [   |   |  ]   |   Y  |   U  |   I  |   O  |   P  |  -   |
  * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
- * | Alt  |   A  |   S  |   D  |   F  |   G  |  (   |   |  )   |   H  |   J  |   K  |   L  |   ;  |  :   |
+ * | Tab  |   A  |   S  |   D  |   F  |   G  |  (   |   |  )   |   H  |   J  |   K  |   L  |   ;  |  :   |
  * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
- * | Sft  |   Z  |   X  |   C  |   V  |   B  |  {   |   |  }   |   N  |   M  |   ,  |   .  |   /  |  \   |
+ * | Ctrl |   Z  |   X  |   C  |   V  |   B  |  {   |   |  }   |   N  |   M  |   ,  |   .  |   /  |  \   |
  * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
- * | Ctrl | GUI  |  App |PrtSc |ESC/  |Space/|Tab/  |   |Back  |Enter/| Del  |   +  |   -  |  *   |  =   |
- * |      |      |      |      |~SYMB |RCtrl |Shift |   |Space |~META |      |      |      |      |      |
+ * | GUI  |   $  |   !  |   ?  |ESC/  |Space/|Tab/  |   |Back  |Enter/| Del  |   +  |   -  |  *   |  =   |
+ * |      |      |      |      |~SYMB |Shift |RCtrl |   |Space |~META |      |      |      |      |      |
  * `------------------------------------------------'   `------------------------------------------------'
  */
 
 [BASE] = KEYMAP( \
-  KC_ESC,   KC_Q,    KC_W,    KC_E,    KC_R,             KC_T,           KC_LBRC,       KC_RBRC,    KC_Y,             KC_U,    KC_I,     KC_O,     KC_P,     KC_MINS, \
-  KC_LALT,  KC_A,    KC_S,    KC_D,    KC_F,             KC_G,           S(KC_9),       S(KC_0),    KC_H,             KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
-  KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,             KC_B,           S(KC_LBRC),    S(KC_RBRC), KC_N,             KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_BSLS, \
-  KC_LCTRL, KC_LGUI, KC_EXLM, S(KC_SLSH), LT(SYMB, KC_ESC), RCTL_T(KC_TAB), SFT_T(KC_SPC), KC_BSPC,    LT(META, KC_ENT), KC_DEL,  KC_AMPR,  KC_PERC,  KC_ASTR,  KC_EQL \
+  KC_ESC,   KC_Q,    KC_W,    KC_E,       KC_R,             KC_T,           KC_LBRC,        KC_RBRC,    KC_Y,             KC_U,    KC_I,     KC_O,     KC_P,     KC_MINS, \
+  KC_TAB,   KC_A,    KC_S,    KC_D,       KC_F,             KC_G,           S(KC_9),        S(KC_0),    KC_H,             KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
+  KC_LCTRL, KC_Z,    KC_X,    KC_C,       KC_V,             KC_B,           S(KC_LBRC),     S(KC_RBRC), KC_N,             KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_BSLS, \
+  KC_LGUI,  KC_DLR,  KC_EXLM, S(KC_SLSH), LT(SYMB, KC_ESC), SFT_T(KC_SPC),  RCTL_T(KC_TAB), KC_BSPC,    LT(META, KC_ENT), KC_DEL,  KC_AMPR,  KC_PERC,  KC_ASTR,  KC_EQL \
 ),
 
 /* META
